5853cd436c691b08183e4673be3f2bbea79a31a4
Cooper Cain <cooper@coopercc.me>
2021-12-03 16:51:15 -0800

    refactor to extract function

--- src/helpers/modelData.ts
+++ src/helpers/modelData.ts
@@ -2,34 +2,45 @@ import { appFiles, FileObject } from './appFiles';
 
 export type ModifiedTreeShape = FileObject[];
 
-export function generateTreeStructure(): ModifiedTreeShape {
-  const resp: ModifiedTreeShape = [];
+const addChildToExistingNode = (
+  fileData: FileObject,
+  parent: FileObject,
+  path: string[]
+) => {
+  //need to nest down structure until we get to the file name
+  for (let i = 1; i < path.length - 1; i++) {
+    //find next level parent from parent.children by name
+    // it will exist so not a worry
+    parent = parent.children?.find(
+      (child) => child.name === path[i]
+    ) as FileObject;
+  }
+
+  if (!parent.children) {
+    parent.children = [];
+  }
+
+  parent.children.push(fileData);
+};
+
+export const generateTreeStructure = (): ModifiedTreeShape => {
+  const tree: ModifiedTreeShape = [];
+  // so we can store where a top level directory is in the tree array
   const addedKeys: { [key: string]: number } = {};
 
   for (const data of Object.keys(appFiles)) {
     const fileData = appFiles[data];
     const path = fileData.fullPath.split('/');
+
     if (path.length === 1) {
-      resp.push(fileData);
-      addedKeys[fileData.name] = resp.length - 1;
+      tree.push(fileData);
+      addedKeys[fileData.name] = tree.length - 1;
     } else {
       let currentDir = path[0];
-      let parent = resp[addedKeys[currentDir]];
-
-      for (let i = 1; i < path.length - 1; i++) {
-        //find parent from parent.children by name
-        parent = parent.children?.find(
-          (child) => child.name === path[i]
-        ) as FileObject;
-      }
-
-      if (!parent.children) {
-        parent.children = [];
-      }
-
-      parent.children.push(fileData);
+      let parent = tree[addedKeys[currentDir]];
+      addChildToExistingNode(fileData, parent, path);
     }
   }
 
-  return resp;
-}
+  return tree;
+};
